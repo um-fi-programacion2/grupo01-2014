3432525343f94bc5e6c9be14d70ca21294e41a4d
Federico Baigorria <federico.baigorria@gmail.com>
2015-03-02 19:08:39 -0300

    Crea Tags de los mensajes. Un mensaje puede contener uno o mas tags.

---
+++ src/test/java/ar/edu/um/canarium/domain/MensajeTagIntegrationTest.java
@@ -0,0 +1,11 @@
+package ar.edu.um.canarium.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = MensajeTag.class)
+public class MensajeTagIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
---
+++ src/test/java/ar/edu/um/canarium/dpmain/MensajeTagDataOnDemand.java
@@ -0,0 +1,6 @@
+package ar.edu.um.canarium.dpmain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = MensajeTag.class)
+public class MensajeTagDataOnDemand {
+}
